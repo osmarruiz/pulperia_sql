32ac7c23d42f23ff5823e51454d7b76a1887d0db
osmarruiz <osmarernesto@gmail.com>
2022-07-05 00:48:18 -0600

    feat: using constraint the key

--- script_pulperia.sql
+++ script_pulperia.sql
@@ -6,6 +6,7 @@ create table vendedor(
     nombre varchar(15),
     apellido varchar(15),
     numero_tel int,
+    constraint vendedorpk primary key(id_vendedor)
 
 );
 create table proveedor(
@@ -13,12 +14,13 @@ create table proveedor(
     nombre_proveedor varchar(15),
     dia_entre enum('lunes','martes','miercoles','jueves','viernes'),
     numero_tel int,
-    
+    constraint proveedorpk primary key(id_proveedor)
 );
 create table cliente(
     id_cliente varchar(10),
     nombre varchar(15),
     apellido varchar(15),
+    constraint clientepk primary key(id_cliente)
 );
 create table producto(
     id_producto varchar(10),
@@ -26,16 +28,25 @@ create table producto(
     cant int,
     tipo enum('comestibles','hogar','materia prima'),
     vencimiento date,
+    constraint productopk primary key(id_producto)
 );
 create table provee(
+    id_provee varchar(5),
     id_proveedor varchar(10),
     id_producto varchar(10),
     fecha_entrega date,
+    constraint proveepk primary key(id_provee),
+    constraint proveefk foreign key(id_proveedor) references proveedor(id_proveedor) on update cascade on delete cascade,
+    constraint proveefk2 foreign key(id_producto) references producto(id_producto) on update cascade on delete cascade,
 );
 create table orden(
+    id_orden varchar(5),
     id_cliente varchar(10),
     id_vendedor varchar(10),
     id_producto varchar(10),
     fecha date,
-
+    constraint ordenpk primary key(id_orden),
+    constraint ordenfk foreign key(id_cliente) references cliente(id_cliente) on update cascade on delete cascade,
+    constraint ordenfk2 foreign key(id_vendedor) references vendedor(id_vendedor) on update cascade on delete cascade,
+    constraint proveefk3 foreign key(id_producto) references producto(id_producto) on update cascade on delete cascade
 );
