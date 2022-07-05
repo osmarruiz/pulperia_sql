3bfa86ca66a8198c08e81dae4e075c95c3a9d573
osmarruiz <osmarernesto17@gmail.com>
2022-07-04 21:16:54 -0600

    feat:create script sql

---
+++ script_pulperia.sql
@@ -0,0 +1,24 @@
+create database if not exits pulperia;
+use pulperia;
+
+create table vendedor(
+
+);
+
+create table proveedor(
+
+);
+
+create table cliente(
+
+);
+
+create table provee(
+
+);
+create table producto(
+
+);
+create table ordenes(
+
+);
