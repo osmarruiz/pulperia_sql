ceb35e8b3df5ac4de895bfeabda2faf5bc46c133
osmarruiz <osmarernesto17@gmail.com>
2022-07-10 20:17:51 -0600

    feat: modificando script_pulperia

--- script_pulperia.sql
+++ script_pulperia.sql
@@ -26,27 +26,39 @@ create table producto(
     id_producto varchar(10),
     nombreprod varchar(10),
     cant int,
+    precio_de_compra int,
+    precio_de_venta int,
+    margen int,
     tipo enum('comestibles','hogar','materia prima'),
-    vencimiento date,
     constraint productopk primary key(id_producto)
 );
-create table provee(
-    id_provee varchar(5),
+create table entrada(
+    id_entrada varchar(5),
     id_proveedor varchar(10),
     id_producto varchar(10),
     fecha_entrega date,
-    constraint proveepk primary key(id_provee),
+    cant int,
+    constraint proveepk primary key(id_entrada),
     constraint proveefk foreign key(id_proveedor) references proveedor(id_proveedor) on update cascade on delete cascade,
-    constraint proveefk2 foreign key(id_producto) references producto(id_producto) on update cascade on delete cascade,
+    constraint proveefk2 foreign key(id_producto) references producto(id_producto) on update cascade on delete cascade
 );
-create table orden(
-    id_orden varchar(5),
+create table venta(
+    id_venta varchar(5),
     id_cliente varchar(10),
     id_vendedor varchar(10),
-    id_producto varchar(10),
     fecha date,
-    constraint ordenpk primary key(id_orden),
-    constraint ordenfk foreign key(id_cliente) references cliente(id_cliente) on update cascade on delete cascade,
-    constraint ordenfk2 foreign key(id_vendedor) references vendedor(id_vendedor) on update cascade on delete cascade,
-    constraint proveefk3 foreign key(id_producto) references producto(id_producto) on update cascade on delete cascade
+    constraint ventapk primary key(id_venta),
+    constraint ventafk foreign key(id_cliente) references cliente(id_cliente) on update cascade on delete cascade,
+    constraint ventafk2 foreign key(id_vendedor) references vendedor(id_vendedor) on update cascade on delete cascade
 );
+create table detalles_de_venta(
+    num_detalle varchar(5),
+    id_venta varchar(10),
+    id_producto varchar(10),
+    cantidad varchar(10),
+    precio int,
+    constraint detallepk primary key(num_detalle),
+    constraint detallefk foreign key(id_venta) references venta(id_venta) on update cascade on delete cascade,
+    constraint detallefk2 foreign key(id_producto) references producto(id_producto) on update cascade on delete cascade
+);
+
